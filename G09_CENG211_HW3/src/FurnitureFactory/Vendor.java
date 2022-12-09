f561475257e2973218949fe52d200762c26c95f7
erol7 <erol7@DESKTOP-7TFPG6L>
2022-12-10 00:01:44 +0300

    vendor

--- G09_CENG211_HW3/src/FurnitureFactory/Vendor.java
+++ G09_CENG211_HW3/src/FurnitureFactory/Vendor.java
@@ -1,5 +1,22 @@
 package FurnitureFactory;
+import java.util.ArrayList;
 
 public class Vendor {
-
+	private ArrayList<Material> materialList;
+	
+	public Vendor() {
+		this.materialList = null;
+	}
+	
+	public Vendor(ArrayList<Material> theMaterialList) {
+		this.materialList = theMaterialList;
+	}
+	
+	public Vendor(Vendor aVendor) {
+		this.materialList = aVendor.materialList;
+	}
+	
+	public ArrayList getMaterialsList() {
+		return this.materialList;
+	}
 }
