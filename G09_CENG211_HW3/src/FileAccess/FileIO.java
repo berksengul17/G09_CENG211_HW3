27df1f1b641fab9aa2b702180c0c47881b9e25ec
Berk Şengül <31830959+berksengul17@users.noreply.github.com>
2022-12-09 23:54:26 +0300

    Create readFile()

--- G09_CENG211_HW3/src/FileAccess/FileIO.java
+++ G09_CENG211_HW3/src/FileAccess/FileIO.java
@@ -1,6 +1,41 @@
 package FileAccess;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class FileIO {
+	
+	public static ArrayList<ArrayList<String>> readFile(String filePath){
+		
+		ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			
+			String line;
+			while((line = br.readLine()) != null) {
+				StringTokenizer tokenizer = new StringTokenizer(line, ",");
+				ArrayList<String> currLine = new ArrayList<String>();
+				while(tokenizer.hasMoreTokens()) {
+					currLine.add(tokenizer.nextToken());
+				}
+				lines.add(currLine);
+			}
+			
+			br.close();
+			
+		} catch(FileNotFoundException e) {
+			System.err.format("Given file does not exist: %s", filePath);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return lines;
+	}
 
 }
  
