f49529c9703d8a9ffa21ddf3754435be66c04a63
Berk Şengül <31830959+berksengul17@users.noreply.github.com>
2022-12-10 21:43:05 +0300

    Comment

--- G09_CENG211_HW3/src/FileAccess/FileIO.java
+++ G09_CENG211_HW3/src/FileAccess/FileIO.java
@@ -13,7 +13,7 @@ public class FileIO {
 	 * Read the file in the given path
 	 * @return ArrayList<ArrayList<String>> containing the read lines
 	 * 			the inner ArrayList holds the data for each line and 
-	 * 			the outer ArrayList holds these lines
+	 * 			the outer ArrayList holds these lines.
 	 */
 	public static ArrayList<ArrayList<String>> readFile(String filePath){
 		
